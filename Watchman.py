565178a92bb3e18a209b83b5484329efd45c0bd4
clense santhosh <64357737+cscreationz@users.noreply.github.com>
2022-09-23 01:03:48 +0530

    Update and rename Password Manager.py to Watchman.py

---
+++ Watchman.py
@@ -0,0 +1,42 @@
+from telnetlib import ENCRYPT
+from cryptography.fernet import Fernet
+
+
+class PasswordManager:
+
+
+    def __init__(self):
+        self.key = None
+        self.password_file = None
+        self.password_dict = {}
+
+    def create_key(self, path):
+        self.key = Fernet.generate_key()
+        with open(path, 'wb') as f:
+            f.write(self.key)
+
+    def load_key(self, path):
+        with open(path, 'rb') as f:
+            self.key = f.read() 
+    def create_password_file(self, path, initial_values=None):
+        self.password_file = path
+
+        if initial_values is not None:
+            for key, value in initial_values.items():
+                pass 
+
+    def load_password_file(self, path):
+        self.password_file = path
+
+        with open(path, 'r') as f:
+            for line in f:
+                site, encrypted = line.split (":")
+                self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()
+    
+    def add_passsword (self, site, password):
+        self.password_dict[site] = password
+
+        if self.password_file is not None:
+            with open(self.password_file, 'a+') as f:
+                encrypted = Fernet(self.key).encrypt(password.encode())
+                f.write(site + ":" + encrypted.decode() + "\n")
